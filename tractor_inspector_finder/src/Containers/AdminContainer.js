f4c1f66d6c10ce7884d2cd17d2563d04c40457b2
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-06-06 22:14:24 +0100

    admin dashboard stays lit when active and added linear gradient to header and footer

--- tractor_inspector_finder/src/Containers/AdminContainer.js
+++ tractor_inspector_finder/src/Containers/AdminContainer.js
@@ -12,6 +12,8 @@ const AdminContainer = () => {
     const [isTractorActive, setIsTractorActive] = useState(false)
     const [isSearchActive, setIsSearchActive] = useState(false)
     const [isInspectorsActive, setIsInspectorsActive] = useState(false)
+  
+    
 
     const handleInspectorClick = (evt) => {
         setIsInspectorActive(!isInspectorActive)
@@ -45,10 +47,10 @@ const AdminContainer = () => {
         <div className="admin-container">
             <h2>Admin Dashboard</h2>
             <div className="admin-card-container">
-                <div className="admin-card" onClick={handleInspectorClick}><h2>Add New Inspector</h2></div>
-                <div className="admin-card" onClick={handleTractorClick}><h2>Add New Tractor</h2></div>
-                <div className="admin-card" onClick={handleSearchClick}><h2>Search for Inspector</h2></div>
-                <div className="admin-card" onClick={handleInspectorsClick}><h2>All Inspectors</h2></div>
+                {isInspectorActive === true ? <div className="admin-card active"  onClick={handleInspectorClick}><h2>Add New Inspector</h2></div>:  <div className="admin-card"  onClick={handleInspectorClick}><h2>Add New Inspector</h2></div>}
+                {isTractorActive === true ? <div className="admin-card active"  onClick={handleTractorClick}><h2>Add New Tractor</h2></div> : <div className="admin-card"  onClick={handleTractorClick}><h2>Add New Tractor</h2></div>}
+                {isSearchActive === true ? <div className="admin-card active"  onClick={handleSearchClick}><h2>Search for Inspector</h2></div> : <div className="admin-card"  onClick={handleSearchClick}><h2>Search for Inspector</h2></div>}
+                {isInspectorsActive === true ? <div className="admin-card active"  onClick={handleInspectorsClick}><h2>All Inspectors</h2></div> : <div className="admin-card"  onClick={handleInspectorsClick}><h2>All Inspectors</h2></div>}
             </div>
             <div className = "admin-form-container">
                 {isInspectorActive === true ? <AddInspectorForm /> : null }
