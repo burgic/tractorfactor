582336a07ecd575a32e5540175f4d9917201bda0
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-06-06 11:50:59 +0100

    added new search ability to home container

--- tractor_inspector_finder/src/Components/TractorLocationForm.js
+++ tractor_inspector_finder/src/Components/TractorLocationForm.js
@@ -39,15 +39,17 @@ const TractorLocationForm = ({handleSearchCode, tractors, handleTractorManufactu
     return (
         <>
             <h2>Search For An Inspector</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="tractor">Manufacturer: </label> 
-                <select onChange={handleChange} name="tractors" id="tractors">
+
+            <form className="home-form" onSubmit={handleSubmit}>
+                <label className="home-form-input left" htmlFor="tractor">Manufacturer: </label> 
+                <select className="home-form-input right" onChange={handleChange} name="tractors" id="tractors">
                     {mappedTractors}
                 </select>
                 <br></br>
-                <label htmlFor="postcode">Postcode: </label> 
-                <input onChange={handlePostcodeChange} type="text" name="postcode" />
-                <input type="submit" value="Search" />
+                <label className="home-form-input left" htmlFor="postcode">Postcode: </label> 
+                <input className="home-form-input right" onChange={handlePostcodeChange} type="text" name="postcode" />
+                <br></br>
+                <input className="button" type="submit" value="Search" />
 
             </form>
             
