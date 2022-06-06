58f6111fb076b8645925fa5dedeaeafa859e98d2
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-06-06 15:12:00 +0100

    list of all inspectors working

---
+++ tractor_inspector_finder/src/Components/InspectorsList.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+
+const InspectorsList = () => {
+
+    const [searchResults, setSearchResults] = useState(null)
+    const [searchResultsMap, setSearchResultsMap] = useState(null)
+
+    useEffect(() => {
+        load()
+    }, [])
+
+    const load = () => {
+        fetch(`http://localhost:8080/inspectors`)
+        .then(res => res.json())
+        .then(data => setSearchResults(data))
+    }
+
+    useEffect(() => {
+        if (searchResults !== null){
+            mapResults()
+        }
+    }, [searchResults])
+
+    const mapResults = () => {
+        const mappedResults = searchResults.map((result, index) => {
+            return <tr><td>{result.name.toUpperCase()}</td><td>{result.postcode}</td><td>{result.address}</td><td>{result.phoneNumber}</td><td>{result.email}</td><button  value={result.id}>Update</button><button  value={result.id}>Delete</button></tr>
+            
+        })
+        setSearchResultsMap(mappedResults)
+    }
+
+    return(
+
+        <>
+        <h3>All Inspectors</h3>
+        {searchResultsMap !== null ? <table><tbody>{searchResultsMap}</tbody></table>: null}
+        </>
+    )
+}
+export default InspectorsList;
